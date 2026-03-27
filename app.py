064792a5fb4bfcaf299bcd9808d6234be4f78368
UniSoloGr <filipp.barkov@helsinki.fi>
2026-03-27 11:41:25 +0000

    Implemented locations as the separate pages.

--- app.py
+++ app.py
@@ -2,15 +2,22 @@ import sqlite3
 from flask import Flask
 from flask import redirect, render_template, request, session
 from werkzeug.security import check_password_hash, generate_password_hash
-import config
-import db
+
+import config, db, users, marketplace
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    locations = marketplace.get_locations()
+    return render_template("index.html", locations=locations)
+
+@app.route("/location/<int:location_id>")
+def show_thread(location_id):
+    thread = forum.get_location(location_id)
+    # messages = forum.get_messages(thread_id)
+    return render_template("thread.html", location=location)
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -21,23 +28,40 @@ def login():
     try:
         password_hash = db.query(sql, [username])[0][0]
     except:
-        return "VIRHE: tyhjä taulukko"
+        return "VIRHE: käyttäjää ei ole olemassa"
 
+    user_id = users.check_login(password_hash, password)
 
-    if check_password_hash(password_hash, password):
+    if user_id:
         session["username"] = username
+        session["user_id"] = user_id
         return redirect("/")
     else:
         return "VIRHE: väärä tunnus tai salasana"
 
 @app.route("/logout")
 def logout():
-    del session["username"]
+    session.clear()
     return redirect("/")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "GET":
+        return render_template("register.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+
+        if password1 != password2:
+            return "VIRHE: salasanat eivät ole samat"
+
+        try:
+            users.create_user(username, password1)
+            return "Tunnus luotu"
+        except sqlite3.IntegrityError:
+            return "VIRHE: tunnus on jo varattu"
 
 @app.route("/create", methods=["POST"])
 def create():
