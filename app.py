b6467e3764796fccab71fde0be494278306c8c93
UniSoloGr <filipp.barkov@helsinki.fi>
2026-03-27 12:15:32 +0000

    working flawlessly

--- app.py
+++ app.py
@@ -14,30 +14,43 @@ def index():
     return render_template("index.html", locations=locations)
 
 @app.route("/location/<int:location_id>")
-def show_thread(location_id):
-    thread = forum.get_location(location_id)
+def show_location(location_id):
+    location = marketplace.get_location(location_id)
     # messages = forum.get_messages(thread_id)
-    return render_template("thread.html", location=location)
+    return render_template("location.html", location=location)
 
-@app.route("/login", methods=["POST"])
+@app.route("/new_location", methods=["POST"])
+def new_location():
+    name = request.form["name"]
+    description = request.form["description"]
+    user_id = session["user_id"]
+
+    location_id = marketplace.add_location(name, description, user_id)
+    return redirect("/location/" + str(location_id))
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    username = request.form["username"]
-    password = request.form["password"]
+    if request.method == "GET":
+        return render_template("login.html")
 
-    sql = "SELECT password_hash FROM Users WHERE username = ?"
-    try:
-        password_hash = db.query(sql, [username])[0][0]
-    except:
-        return "VIRHE: käyttäjää ei ole olemassa"
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
 
-    user_id = users.check_login(password_hash, password)
+        sql = "SELECT password_hash FROM Users WHERE username = ?"
+        try:
+            password_hash = db.query(sql, [username])[0][0]
+        except:
+            return "VIRHE: käyttäjää ei ole olemassa"
 
-    if user_id:
-        session["username"] = username
-        session["user_id"] = user_id
-        return redirect("/")
-    else:
-        return "VIRHE: väärä tunnus tai salasana"
+        user_id = users.check_login(username, password)
+
+        if user_id:
+            session["username"] = username
+            session["user_id"] = user_id
+            return redirect("/")
+        else:
+            return "VIRHE: väärä tunnus tai salasana"
 
 @app.route("/logout")
 def logout():
