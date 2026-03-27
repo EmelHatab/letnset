28d8051563d8806b7b3271f6be86cca427f90853
UniSoloGr <filipp.barkov@helsinki.fi>
2026-03-27 14:31:09 +0000

    Clean up

--- marketplace.py
+++ marketplace.py
@@ -1,7 +1,7 @@
 import db
 
 def get_locations():
-    sql = """SELECT l.id, l.name, l.description, u.username
+    sql = """SELECT l.id, l.name, l.description, l.image, u.username
              FROM locations l, users u
              WHERE l.user_id = u.id
              GROUP BY l.id
@@ -9,7 +9,9 @@ def get_locations():
     return db.query(sql)
 
 def get_location(location_id):
-    sql = "SELECT id, name FROM locations WHERE id = ?"
+    sql = """SELECT l.id, l.name, l.description, l.user_id, l.image, u.username
+             FROM locations l, users u
+             WHERE l.user_id = u.id AND l.id = ?"""
     return db.query(sql, [location_id])[0]
 
 def get_messages(thread_id):
@@ -23,9 +25,9 @@ def get_message(message_id):
     sql = "SELECT id, content, user_id, thread_id FROM messages WHERE id = ?"
     return db.query(sql, [message_id])[0]
 
-def add_location(name, description, user_id):
-    sql = "INSERT INTO locations (name, description, user_id) VALUES (?, ?, ?)"
-    db.execute(sql, [name, description, user_id])
+def add_location(name, description, user_id, image_data=None):
+    sql = "INSERT INTO locations (name, description, user_id, image) VALUES (?, ?, ?, ?)"
+    db.execute(sql, [name, description, user_id, image_data])
     location_id = db.last_insert_id()
     # add_message(content, user_id, thread_id)
     return location_id 
@@ -35,10 +37,14 @@ def add_message(content, user_id, thread_id):
              (?, datetime('now'), ?, ?)"""
     db.execute(sql, [content, user_id, thread_id])
 
-def update_message(message_id, content):
-    sql = "UPDATE messages SET content = ? WHERE id = ?"
-    db.execute(sql, [content, message_id])
+def update_location(location_id, description, image_data=None):
+    if image_data:
+        sql = "UPDATE Locations SET description = ?, image = ? WHERE id = ?"
+        db.execute(sql, [description, image_data, location_id])
+    else:
+        sql = "UPDATE Locations SET description = ? WHERE id = ?"
+        db.execute(sql, [description, location_id])
 
-def remove_message(message_id):
-    sql = "DELETE FROM messages WHERE id = ?"
-    db.execute(sql, [message_id])
+def remove_location(location_id):
+    sql = "DELETE FROM locations WHERE id = ?"
+    db.execute(sql, [location_id])
