b384456078afc599fc7b1752ec98d798fc9789f8
UniSoloGr <filipp.barkov@helsinki.fi>
2026-03-29 21:03:49 +0000

    Added message support!

--- marketplace.py
+++ marketplace.py
@@ -14,15 +14,18 @@ def get_location(location_id):
              WHERE l.user_id = u.id AND l.id = ?"""
     return db.query(sql, [location_id])[0]
 
-def get_messages(thread_id):
-    sql = """SELECT m.id, m.content, m.sent_at, m.user_id, u.username
-             FROM messages m, users u
-             WHERE m.user_id = u.id AND m.thread_id = ?
-             ORDER BY m.id"""
-    return db.query(sql, [thread_id])
+def get_comments(location_id):
+    sql = """SELECT c.id, c.comment, c.sent_at, c.user_id, u.username
+             FROM comments c, users u
+             WHERE c.user_id = u.id AND c.location_id = ?
+             ORDER BY c.id"""
+    try:
+        return db.query(sql, [location_id])
+    except:
+        return []
 
 def get_message(message_id):
-    sql = "SELECT id, content, user_id, thread_id FROM messages WHERE id = ?"
+    sql = "SELECT id, comment, user_id, location_id FROM messages WHERE id = ?"
     return db.query(sql, [message_id])[0]
 
 def add_location(name, description, user_id, image_data=None):
@@ -32,10 +35,10 @@ def add_location(name, description, user_id, image_data=None):
     # add_message(content, user_id, thread_id)
     return location_id 
 
-def add_message(content, user_id, thread_id):
-    sql = """INSERT INTO messages (content, sent_at, user_id, thread_id) VALUES
-             (?, datetime('now'), ?, ?)"""
-    db.execute(sql, [content, user_id, thread_id])
+def add_comment(content, user_id, location_id):
+    sql = """INSERT INTO Comments (comment, user_id, location_id, sent_at) VALUES
+             (?, ?, ?, datetime('now'))"""
+    db.execute(sql, [content, user_id, location_id])
 
 def update_location(location_id, name, description, image_data=None):
     if image_data:
